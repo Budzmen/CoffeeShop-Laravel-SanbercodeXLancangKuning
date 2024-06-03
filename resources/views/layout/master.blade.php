7e10d271f28f214b7b7194bb95de3276d10c5076
Muhammad Alvin Budiman <komogonz123@gmail.com>
2024-06-03 19:40:31 +0700

    Implementasi template ke project laravel

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Coffo</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" type="text/css" href="{{asset('template/css/bootstrap.min.css')}}">
+      <!-- style css -->
+      <link rel="stylesheet" type="text/css" href="{{asset('template/css/style.css')}}">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="{{asset('template/css/responsive.css')}}">
+      <!-- fevicon -->
+      <link rel="icon" href="{{asset('images/fevicon.png')}}" type="image/gif" />
+      <!-- font css -->
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;800&display=swap" rel="stylesheet">
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="{{asset('template/css/jquery.mCustomScrollbar.min.css')}}">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+   </head>
+   <body>
+      <!-- header -->
+      @include ('partial.nav')
+
+      <!-- banner section start --> 
+      @yield ('content')
+
+      <!-- footer section start -->
+      @include ('partial.footer')
+
+      <!-- Javascript files-->
+      <script src="{{asset('template/js/jquery.min.js')}}"></script>
+      <script src="{{asset('template/js/popper.min.js')}}"></script>
+      <script src="{{asset('template/js/bootstrap.bundle.min.js')}}"></script>
+      <script src="{{asset('template/js/jquery-3.0.0.min.js')}}"></script>
+      <script src="{{asset('template/js/plugin.js')}}"></script>
+      <!-- sidebar -->
+      <script src="{{asset('template/js/jquery.mCustomScrollbar.concat.min.js')}}"></script>
+      <script src="{{asset('template/js/custom.js')}}"></script>
+   </body>
+</html>
