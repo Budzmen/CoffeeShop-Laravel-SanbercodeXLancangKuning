14b2d472ec91d98be23796ed41efee960a633a32
Marigel03 <kigentzy@gmail.com>
2024-06-04 18:38:22 +0700

    Menambahkan file migration

--- database/migrations/2024_06_04_105018_create_product_table.php
+++ database/migrations/2024_06_04_105018_create_product_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('product', function (Blueprint $table) {
-            $table->id();
+            $table->id()->notnull();
             $table->char('nama',50);
             $table->string('deskripsi',45);
             $table->decimal('harga',45);
