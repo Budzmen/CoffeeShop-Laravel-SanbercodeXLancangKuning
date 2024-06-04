16a0c3598649cb82a9ba7cb893ea4715fc88dce7
Marigel03 <kigentzy@gmail.com>
2024-06-04 17:24:02 +0700

    migration

--- database/migrations/2024_06_04_095657_create_pelanggan_tabel.php
+++ database/migrations/2024_06_04_095657_create_pelanggan_tabel.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('pelanggan', function (Blueprint $table) {
             $table->id();
+            $table->char('nama', 50);
+            $table->integer('umur', 2);
+            $table->string('email', 45);
+            $table->string('password', 45);
             $table->timestamps();
         });
     }
