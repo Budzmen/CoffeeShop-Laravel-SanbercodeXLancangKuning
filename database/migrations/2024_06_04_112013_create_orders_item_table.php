e718e1ebc8aee7518c947264fb790ae5f0a2c7c8
Muhammad Alvin Budiman <komogonz123@gmail.com>
2024-06-05 11:28:25 +0700

    perubahan migration orders item

--- database/migrations/2024_06_04_112013_create_orders_item_table.php
+++ database/migrations/2024_06_04_112013_create_orders_item_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('orders_item', function (Blueprint $table) {
             $table->id()->notnull();
-            $table->unsignedBigInteger('pelanggan_id')->notnull();
-            $table->foreign('pelanggan_id')->references('id')->on('pelanggan')->onDelete('cascade');
+            $table->unsignedBigInteger('order_id')->notnull();
+            $table->foreign('order_id')->references('id')->on('order')->onDelete('cascade');
 
             $table->unsignedBigInteger('product_id')->notnull();
             $table->foreign('product_id')->references('id')->on('product')->onDelete('cascade');
